61f08b0cbaa2b28a3c58904a255b84599113f4e3
Frost Lee <canchen.lee@gmail.com>
2019-10-06 23:16:34 -0400

    animation fix.

--- N U H M.playground/Sources/ValueView.swift
+++ N U H M.playground/Sources/ValueView.swift
@@ -2,23 +2,29 @@ import UIKit
 
 public class ValueView: UIView {
     
-    public var lowView: UIView = UIView()
-    public var highView: UIView = UIView()
+    public var lowView: UIView = {
+        let view = UIView()
+        view.backgroundColor = barColor
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        return view
+    }()
+    public var highView: UIView = {
+        let view = UIView()
+        view.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.15)
+        view.translatesAutoresizingMaskIntoConstraints = false
+        view.clipsToBounds = true
+        return view
+    }()
     public var valueConstraint: NSLayoutConstraint!
     public var zeroOffset: CGFloat = 16
     private var value: Double = 0
     
-    public init() {
-        super.init(frame: CGRect.zero)
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
         
         self.addSubview(lowView)
         self.addSubview(highView)
-        self.backgroundColor = backgroundColor
-        
-        lowView.translatesAutoresizingMaskIntoConstraints = false
-        lowView.backgroundColor = barColor
-        highView.translatesAutoresizingMaskIntoConstraints = false
-        highView.backgroundColor = #colorLiteral(red: 0.9999960065, green: 1, blue: 1, alpha: 0.15)
         
         NSLayoutConstraint.activate([
             highView.topAnchor.constraint(equalTo: self.topAnchor, constant: 0),
@@ -33,10 +39,9 @@ public class ValueView: UIView {
         valueConstraint = lowView.heightAnchor.constraint(equalToConstant: zeroOffset)
         valueConstraint.isActive = true
         
+        self.backgroundColor = backgroundColor
         self.clipsToBounds = true
-        self.highView.clipsToBounds = true
-        self.lowView.clipsToBounds = true
-        layoutSubviews()
+        
     }
     
     public required init?(coder aDecoder: NSCoder) {
@@ -56,23 +61,30 @@ public class ValueView: UIView {
     
     public func changeValue(to value: Double, animated: Bool = true) {
         self.value = value
-        valueConstraint.constant = (self.frame.height - zeroOffset) * CGFloat(value) + zeroOffset
-        self.setNeedsUpdateConstraints()
-        if animated {
-            UIView.animate(withDuration: timeInterval, delay: 0, options: .curveEaseInOut,
-                           animations: {self.layoutIfNeeded()}, completion: nil)
-        } else {
-            UIView.animate(withDuration: 0, delay: 0, options: .curveLinear,
-                           animations: {self.layoutIfNeeded()}, completion: nil)
-        }
+        UIView.animate(
+            withDuration: timeInterval,
+            delay: 0,
+            options: .curveEaseInOut,
+            animations: {
+                self.valueConstraint.constant = (self.frame.height - self.zeroOffset) * CGFloat(value) + self.zeroOffset
+                self.layoutIfNeeded()
+            },
+            completion: nil
+        )
     }
     
     public func reset() {
         self.value = 0
-        valueConstraint.constant = zeroOffset
-        self.setNeedsUpdateConstraints()
-        UIView.animate(withDuration: timeInterval, delay: 0, options: .curveEaseInOut,
-                       animations: {self.layoutIfNeeded()}, completion: nil)
+        UIView.animate(
+            withDuration: timeInterval,
+            delay: 0,
+            options: .curveEaseInOut,
+            animations: {
+                self.valueConstraint.constant = self.zeroOffset
+                self.layoutIfNeeded()
+            },
+            completion: nil
+        )
     }
     
 }
